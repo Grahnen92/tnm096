ae650eb64cbf45ce19f77e97ec2190f68c0f6c7f
ronjagrosz <ronja_ronja@msn.com>
2016-05-17 19:44:34 +0200

    Maybe done with lab 2

--- lab2a/lab2a/src/Sentence.java
+++ lab2a/lab2a/src/Sentence.java
@@ -5,52 +5,137 @@ import java.util.List;
 
 
 public class Sentence {
-	String s;
+	ArrayList<String> s;
 	
 	public Sentence(){
-		s = "";
+		s = new ArrayList<String>();
 	}
 	
 	public void addToSentence(String c){
-		s += c;
+		s.add(c);
 	}
 	
 	// Change from boolean to clause
 	public void evaluate() {
 		//char test = s.charAt(0);
-		String newS = "";
+		ArrayList<String> newS = new ArrayList<String>();
 		String converted;
-		int start, end;
 		
-		start = 0;
-		
-		//for(int i = 0; i < s.length(); i++){
-		//	while(true){
-		do{
-			end = start +1;
-			while(s.charAt(end) != ')'){
-				end++;
+		for(int i = 0; i < s.size(); i++){
+
+			converted = clean(s.get(i));
+			
+			//check if the new clause is empty && check if the new clause is already in the sentence
+			if(!converted.isEmpty() && !contains(converted, newS)){
+				
+				newS.add(converted);	
 			}
-			
-			converted = resolve(s.substring(start + 1, end));
-			if(converted != "")
-				newS += "(" + converted + ")";
-			
-			if(end != s.length() - 1)
-				newS += s.charAt(end+1);
-			else
-				break;
-			
-			
-			start = end + 2;
-		} while(end != s.length() - 1);
+		}
 		
 		s = newS;
+		
+		
+		print();
+		System.out.println("clean");
+		
+		// Evaluate resolution
+		for(int i = 0; i < s.size(); i++){
+			for(int j = 0; j < s.size(); j++){
+				
+				if(i == j)
+					continue;
+				
+				System.out.println("indicies: " + i + " " + j);
+				
+				resolve(s.get(i), s.get(j));
+			}
+		}
+		
+		System.out.println("finding subsets\n");
+		
+		int sSize;
+		String tmpString;
+		for(int i = 0; i < s.size(); i++)
+		{
+			System.out.print( i + "\nbf: ");
+			print();
+			sSize = s.size();
+			tmpString = s.get(i); 
+			subsetRm(tmpString);
+			s.add(tmpString);
+			if(sSize - s.size() != 0 )
+				i = 0; 
+			
+			System.out.print("af: ");
+			print();
+			System.out.println("");
+		}
+		
 	}
 	
-	public String resolve(String _input) {
+	public void resolve(String m, String n) {
+		int c1 = 0, ml = m.length();
+		Boolean not;
+		System.out.print("resolving: ");
+		print();
+		
+		//walk through the entire m sentence
+		while(c1 < m.length()) {
+			//find the next non operand
+			while(c1 < m.length() && (m.charAt(c1) == '|' || m.charAt(c1) == '~') ){
+				c1++;
+			}
+			String ch = "" + m.charAt(c1);
+			if(c1 != 0 && m.charAt(c1-1) == '~'){
+				if(n.contains(ch + "")){
+					n = n.replace(ch + "|", "");
+					if(c1 < m.length()) n = n.replace("|" + ch, "");
+					if(c1 < m.length()) n = n.replace(ch + "", "");
+					if(c1 < m.length()) m = m.replace("~" + ch + "|", "");
+					if(c1 < m.length()) m = m.replace("|~" + ch, "");
+					if(c1 < m.length()) m = m.replace("~" + ch, "");
+				} else c1++;
+				
+			}
+			else{
+				if(n.contains("~" + ch)){
+					n = n.replace("~" + ch + "|", ""); 
+					if(c1 < m.length()) n = n.replace("|~" + ch, "");
+					if(c1 < m.length()) n = n.replace("~" + ch, "");
+					if(c1 < m.length()) m = m.replace( ch + "|", "");
+					if(c1 < m.length()) m = m.replace("|" + ch, "");
+					if(c1 < m.length()) m = m.replace(ch + "", "");
+				}
+				c1++;
+			}
+		}
+			
+		System.out.println("m,n " + m + " " + n);
+
+		//clean strings 
+		if(ml != m.length()){
+			if(!n.isEmpty() && !m.isEmpty()){
+				if(subset(n,m) )
+					s.add(n);
+				else if(subset(m,n))
+					s.add(m);
+				else
+					s.add(n + "|" + m);
+			}
+			else if (!n.isEmpty()){
+				//subsetRm(n);
+				s.add(n);
+			}
+			else if(!m.isEmpty()){
+				//subsetRm(m);
+				s.add(m);
+			}	
+		}
+	}
 	
-		System.out.println(_input);
+	public String clean(String _input) {
+	
+		System.out.println("input: " + _input);
 		int c1 = 0;
 		Boolean not;
 		String newS = ""; 
@@ -58,9 +143,8 @@ public class Sentence {
 		//walk through the entire sentence
 		while(c1 < _input.length()){
 			
-			System.out.println("c1: " + c1);
 			//find the next non operand
-			while(_input.charAt(c1) == '|' && c1 == '~' && c1 < _input.length()){
+			while(c1 < _input.length() && (_input.charAt(c1) == '|' || _input.charAt(c1) == '~') ){
 				c1++;
 			}
 			if(c1 != 0 && _input.charAt(c1-1) == '~')
@@ -70,14 +154,12 @@ public class Sentence {
 			
 			//walk through the sentence and look for duplicates
 			for(int i = c1 + 1; i < _input.length(); i++){
-			      System.out.println(i);
-
+			      
 				if(_input.charAt(c1) == _input.charAt(i)){
 					//A | ~A, remove all A
 					if(_input.charAt(i - 1) == '~' && !not){
 						_input = _input.substring(0, i - 2 ) + _input.substring(i+1, _input.length() );
 						i = i - 2;
-						System.out.println(i + " " + _input.length());
 						for(int j = i; j < _input.length(); j++){
 							if(_input.charAt(j) == _input.charAt(c1)){
 								if(_input.charAt(c1 - 1) == '~' ){
@@ -90,13 +172,11 @@ public class Sentence {
 								}
 							}
 						}
-						System.out.println("debug");
+						// Remove the first instance of the character
 						if(c1 != 0)
-							_input = _input.substring(0, i - 1 ) + _input.substring(i+1, _input.length() );
-						else if(i < _input.length())
-							_input = _input.substring(i+1, _input.length() );
-						
-						System.out.println("debug");
+							_input = _input.substring(0, c1 - 1 ) + _input.substring(c1 + 1, _input.length() );
+						else if(c1 < _input.length())
+							_input = _input.substring(c1 + 1, _input.length() );
 						break;
 					}
 					//A | A, remove duplicate
@@ -127,11 +207,12 @@ public class Sentence {
 							}
 						}
 						
-						if(c1 != 0)
-							_input = _input.substring(0, i - 2 ) + _input.substring(i+1, _input.length() );
+						// Remove the first instance of the character
+						if(c1 != 0) {
+							_input = _input.substring(0, c1 - 1 ) + _input.substring(c1+2, _input.length() );
+						}
 						else if(c1 < _input.length())
-							_input = _input.substring(i+1, _input.length() );
-						
+							_input = _input.substring(c1+1, _input.length() );
 						break;
 					}	
 				}			
@@ -139,10 +220,73 @@ public class Sentence {
 			c1++;
 		}
 		
+		System.out.println("output: " + _input + "\n");
+
 		return _input;
 	}
 	
-	public void print() {
-		 System.out.println(s);
+	public Boolean contains(String c, ArrayList<String> _al){
+		
+		for (int i = 0; i < _al.size(); i++){
+			if(_al.get(i).equals(c) )
+				return true;
+		}
+		
+		return false;
+	}
+	
+	public void subsetRm(String c){
+		Boolean isSubset = true;
+		// Loop through clauses
+		for(int i = 0; i < s.size(); i++){
+			// Loop through letters in clause c
+			for(int j = 0; j < c.length(); j++){
+				if(String.valueOf(c.charAt(j)).equals("|") || String.valueOf(c.charAt(j)).equals("~") )
+					continue;
+				
+				if(j != 0 && c.charAt(j - 1) == '~'){
+					if( !s.get(i).contains(String.valueOf(c.charAt(j - 1)) + String.valueOf(c.charAt(j))) )
+						isSubset = false;
+				}
+				else if( !s.get(i).contains(String.valueOf(c.charAt(j))) ){
+					isSubset = false;
+				}
+				
+			}
+			if(isSubset){
+				s.remove(i);
+				if(s.size() > 0)
+					i--;
+			}
+				
+			
+			isSubset = true;
+		}
+	}
+	
+	public Boolean subset(String c, String s){
+		// Loop through letters in clause c
+		for(int j = 0; j < c.length(); j++){
+			if(String.valueOf(c.charAt(j)).equals("|") || String.valueOf(c.charAt(j)).equals("~") )
+				continue;
+			
+			if(j != 0 && c.charAt(j - 1) == '~'){
+				if( !s.contains(String.valueOf(c.charAt(j - 1)) + String.valueOf(c.charAt(j))) )
+					return false;
+			}
+			else if( !s.contains(String.valueOf(c.charAt(j))) ){
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public void print() {	
+		for(int i = 0; i < s.size() - 1; i++){
+			System.out.print("(" + s.get(i) + ")&") ;
+		}
+		
+		 System.out.println("("+s.get(s.size() - 1) + ")");
 	}
 }
--- lab2a/lab2a/src/main.java
+++ lab2a/lab2a/src/main.java
@@ -8,10 +8,26 @@ public class main {
 	public static void main(String [ ] args)
 	{     	      
 	      Sentence sTest = new Sentence();
-	      sTest.addToSentence("(a|~a)&(c|d|e|f)&(g|h|i)");
-	      sTest.print();
+	      //sTest.addToSentence("a|~a");
+		  //sTest.addToSentence("d|e|f|~c");
+		  //sTest.addToSentence("b|a|c");
+		  //sTest.addToSentence("d|~c");
+	      sTest.addToSentence("s|n");
+	      sTest.addToSentence("~s");
+	      sTest.addToSentence("p|n");
+	      sTest.addToSentence("~n|p");
+	      //sTest.addToSentence("s");
+	      
+		  /*sTest.addToSentence("~g|h|g|i|~g");
+		  sTest.addToSentence("~j|~j");
+	      sTest.addToSentence("~m");
+		  sTest.addToSentence("~k|~k");
+	      sTest.addToSentence("h|i");
+		  sTest.addToSentence("h");
+		  sTest.addToSentence("~c|~h");*/
+		  sTest.print();
 	      sTest.evaluate();
 	      sTest.print();
-	      System.out.println("success");
+	      System.out.println("success!");
 	}
 }
